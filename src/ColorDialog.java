1a0f41ae77971fedeb84336c98a2b0980849fcd6
Dominik D. Geyer <dominik.geyer@gmail.com>
2010-07-15 18:25:59 +0000

    category chooser: changed available colors; added option for choosing custom color

--- src/ColorDialog.java
+++ src/ColorDialog.java
@@ -5,35 +5,45 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 
 @SuppressWarnings("serial")
 public class ColorDialog extends JDialog implements ActionListener {
 	private static final String NOCOLOR = "nocolor";
+	private static final String CHOOSER = "chooser";
 	private static final int SIZE = 32;
 	
 	private Color selectedColor;
+	private Color currentColor;
 	
-	public ColorDialog(JFrame parent, Point position, Color currentColor /*not used yet*/) {
+	public ColorDialog(JFrame parent, Point position, Color currentColor) {
 		super(parent);
 		
 		this.setModal(true);
 		this.setUndecorated(true);
 		this.setLocation(position);
 		
+		this.currentColor = currentColor;
+		
+		
 		Color[] colors = new Color[] {
 				new Color(219,	148,	112),
 				new Color(219,	201,	112),
-				new Color(184,	219,	112),
+				//new Color(184,	219,	112),
 				new Color(130,	219,	112),
-				new Color(219,	112,	130),
-				new Color(207,	111,	63),
-				new Color(112,	184,	219),
+				new Color(229,	122,	194),
+				//new Color(207,	111,	63),
+				new Color(122,	194,	229),
+				new Color(205,	205,	205),
 		};
 		
 		JPanel panel = new JPanel();
@@ -57,10 +67,27 @@ public class ColorDialog extends JDialog implements ActionListener {
 		}
 		
 		this.add(panel);
+
+		
+		// color-chooser button
+		btn = new JButton("\u2661");  //2665");
+		btn.setPreferredSize(new Dimension(SIZE, SIZE));
+		btn.setMinimumSize(new Dimension(SIZE, SIZE));
+		btn.setMaximumSize(new Dimension(SIZE, SIZE));
+		btn.setMargin(new Insets(0, 0, 0, 0));
+		
+		btn.setBackground(Color.WHITE);
+		btn.setForeground(Color.BLUE);
+		btn.setFont(btn.getFont().deriveFont(22.0f));
+		
+		btn.addActionListener(this);
+		btn.setActionCommand(CHOOSER);
+		
+		panel.add(btn);
 		
 		
 		// no-color button
-		btn = new JButton("\u2022");
+		btn = new JButton("\u2612");
 		btn.setPreferredSize(new Dimension(SIZE, SIZE));
 		btn.setMinimumSize(new Dimension(SIZE, SIZE));
 		btn.setMaximumSize(new Dimension(SIZE, SIZE));
@@ -68,12 +95,26 @@ public class ColorDialog extends JDialog implements ActionListener {
 		
 		btn.setBackground(Color.WHITE);
 		btn.setForeground(Color.RED);
-		btn.setFont(btn.getFont().deriveFont(20.0f));
+		btn.setFont(btn.getFont().deriveFont(22.0f));
 		
 		btn.addActionListener(this);
 		btn.setActionCommand(NOCOLOR);
 		
 		panel.add(btn);
+		
+		
+		// exit on key ESC
+		final KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, true);
+		getRootPane().registerKeyboardAction(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ColorDialog.this.selectedColor = ColorDialog.this.currentColor;
+				
+				ColorDialog.this.setVisible(false);
+				ColorDialog.this.dispose();
+			}},
+			keyStroke,
+			JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		
 		
 		this.pack();
@@ -83,10 +124,20 @@ public class ColorDialog extends JDialog implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		JButton btn = (JButton) e.getSource();
 		
-		if (e.getActionCommand() == NOCOLOR)
+		if (e.getActionCommand() == NOCOLOR) {
 			this.selectedColor = null;
-		else
+		} else if (e.getActionCommand() == CHOOSER) {
+			Color chosenColor = JColorChooser.showDialog(ColorDialog.this,
+					"Choose a custom color",
+					ColorDialog.this.currentColor);
+			
+			if (chosenColor != null)
+				this.selectedColor = chosenColor;
+			else
+				this.selectedColor = this.currentColor;
+		} else {
 			this.selectedColor = btn.getBackground();
+		}
 		
 		this.setVisible(false);
 		this.dispose();
