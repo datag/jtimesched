1a7e4fee33c6d6285029594c28fd9fccbacb399f
Dominik D. Geyer <dominik.geyer@gmail.com>
2013-02-11 17:27:23 +0100

    Fixed launcher script
    Change into script directory before executing JAR. This change
    requires bash as shell.
    
    Fixes issue #2 reported by @opice.

--- launcher/jTimeSched.sh
+++ launcher/jTimeSched.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -n "$JAVA_HOME" ]; then
-	JAVA=$JAVA_HOME/bin/java	# use $JAVA_HOME if set
+if [[ -n "$JAVA_HOME" ]]; then
+	JAVA="$JAVA_HOME/bin/java"	# use $JAVA_HOME if set
 else
-	JAVA=java		# java binary within the current PATH
+	JAVA=java	# java binary within the current PATH
 fi
+
+# change into launcher directory
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 if ! $JAVA -jar jTimeSched.jar "$@"; then
 	echo "Error executing Java binary." >&2
 	exit 1
 fi
-
